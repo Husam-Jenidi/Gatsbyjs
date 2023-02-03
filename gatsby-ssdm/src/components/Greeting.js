1e4401c6555db1ca804f84ea5a6fe84361e16810
Husam Jenidi <117489131+Husam-Jenidi@users.noreply.github.com>
2023-02-03 18:31:45 +0100

    testing

---
+++ gatsby-ssdm/src/components/Greeting.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+const greeting = (sdd) => {
+return (
+    <p1>Hi {sdd.nameSSS1}</p1>
+)
+}
+export default greeting
---
+++ gatsby-ssdm/src/pages/about.js
@@ -0,0 +1,18 @@
+
+import Greeting from "../components/Greeting"
+import * as React from 'react'
+
+// Rendering the <Greeting> component
+const SayHello = () => {
+    return (
+        <div>
+            <Greeting name1="dsdASDASDsdn" />
+            <Greeting nameSSS1="Obinnasdasda" />
+            <Greeting name1="Geneasdasrosa" />
+        </div>
+    )
+}
+
+export default SayHello
+export const Head = () => <title>About Me</title>
+
