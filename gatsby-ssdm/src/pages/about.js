d000b606172d48f65c165d8d76343e2c2ae06760
Husam Jenidi <117489131+Husam-Jenidi@users.noreply.github.com>
2023-02-04 16:50:54 +0100

    finishing part 2

--- gatsby-ssdm/src/pages/about.js
+++ gatsby-ssdm/src/pages/about.js
@@ -2,16 +2,19 @@
 import Greeting from "../components/Greeting"
 import * as React from 'react'
 import {Link} from "gatsby";
+import Layout from "../components/layout";
 
 // Rendering the <Greeting> component
 const SayHello = () => {
     return (
+        <Layout pageTitle="About page">
         <div>
             <Greeting name="Husa" />
             <Greeting name="sham" />
             <Greeting name="sana" />
             <h1><Link to="../">home</Link> </h1>
         </div>
+        </Layout>
     )
 }
 
