37b1525773aa2a13bd4ab118f075fcb64f45407c
Husam Jenidi <117489131+Husam-Jenidi@users.noreply.github.com>
2023-02-05 18:34:20 +0100

    noting important

--- gatsby-ssdm/gatsby-config.js
+++ gatsby-ssdm/gatsby-config.js
@@ -3,19 +3,19 @@
  */
 /*after any change to this file your need to restart the server again*/
 module.exports = {
-  siteMetadata: {
-      title:"Gatsby-SSD",
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
-      {
-          resolve: "gatsby-source-filesystem",
-          options: {
-              name: `blog`,
-              path: `${__dirname}/blog`,
-          }
-      },
-  ],
-}
+    siteMetadata: {
+        title:"Gatsby-SSD",
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+            }
+        },
+    ],
+};
